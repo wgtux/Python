7aec97f990c3a3a4b97a29990d9b0fa8789c7129
Weslen Gomes <weslengomes@gmail.com>
2017-08-14 15:01:11 -0300

    Melhorando o exercicio

--- CursoEmVideo/desafio33.py
+++ CursoEmVideo/desafio33.py
@@ -4,6 +4,17 @@ Leia 3 numeros e mostre o maior e o menor
 n1 = int(input('Digite o primeiro numero: '))
 n2 = int(input('Digite o segundo numero: '))
 n3 = int(input('Digite o terceiro numero: '))
-
-m = max(n1,n2,n3)
-print('O maior numero é o: ',m)
+#Verificando o menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+#Maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O menor numero é o {}'.format(menor))
+print('O maior numero é o {}'.format(maior))
