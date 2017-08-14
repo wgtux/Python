6afa133f73eb2cd4c92c425806fbfd6e63ca7439
Weslen Gomes <weslengomes@gmail.com>
2017-08-14 16:19:04 -0300

    melhorias

--- CursoEmVideo/desafio34.py
+++ CursoEmVideo/desafio34.py
@@ -4,8 +4,8 @@ Inferior a esse valor 15%
 '''
 s = float(input('Digite o salario: R$'))
 if s > 1250:
-    nv = (s*0.10)+s
+    nv = s +(s*0.10)
     print('Seu novo salrio com aumento de 10% é de R$ {}'.format(nv))
 else:
-    nv = (s*0.15)+s
-    print('Seu novo salrio com aumento de 10% é de R$ {}'.format(nv))
+    nv = s + (s*0.15)
+    print('Seu novo salario com aumento de 10% é de R$ {}'.format(nv))
